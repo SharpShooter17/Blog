85174f4ae6e2f51aa560fd4b66e941048e7ba87c
Bartek <b.ujazdowski@gmail.com>
2017-12-02 13:46:32 +0100

    Revert "front"

--- front/src/components/Api.js
+++ front/src/components/Api.js
@@ -1,69 +1,13 @@
+import React, {Component} from 'react'
 import axios from 'axios';
-import Cookies from 'js-cookie'
 var querystring = require('querystring');
 
-const apiClient = function() {
-    return axios.create({
-      baseURL: "http://localhost:80/Blog/", //http://localhost:80/Blog/
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      }
-    });
+export const apiClient = axios.create({
+  baseURL: "http://localhost:80/Blog/", //http://localhost:80/Blog/
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
   }
+});
 
-const getBlogCategories = function(obj){
-    return apiClient().get('/index.php?/API/BlogCategoryController/getCategories')
-     .then(response => {
-       obj.setState({
-         blog_categories:  response.data.results
-       })
-     })
-     .catch(error => {
-       console.log(error);
-     })
-  }
-
-const getUserBlogs = function(obj){
-  axios.post('http://localhost/Blog/index.php?/API/BlogController/getUserBlogs', querystring.stringify({
-      token: Cookies.get('token')
-  }))
-    .then(response => {
-      obj.setState({
-        blogs: response.data.results
-      })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-const getCategories = function (obj, blogId){
-    apiClient().get('/index.php?/API/CategoryController/getBlogCategories/' + blogId)
-    .then(response => { console.log('kategorie response', response.data.results)
-      obj.setState({
-        categories: response.data.results
-      })
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-const addBlog = function (obj, blogName, blogCategory){
-  axios.post('http://localhost/Blog/index.php?/API/BlogController/addBlog', querystring.stringify({
-      token: Cookies.get('token'),
-      name: blogName,
-      category: blogCategory
-  }))
-    .then(response => {
-      obj.setState({
-        msg: response.data.results
-      })
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-
-export default {getBlogCategories, getUserBlogs, getCategories}
+export default apiClient
